2aadc975a9846b733dca3d0dbb5b0f918ea3ff9c
lop19029 <alexhumbertolds@gmail.com>
2021-06-01 21:47:50 -0300

    Add client side validation to register view

--- phpmotors/view/register.php
+++ phpmotors/view/register.php
@@ -26,17 +26,17 @@
 
                     <p class="info-note">*All the fields are required.</p>
                     <label for="clientFirstname"><b>First name</b></label><br>
-                    <input type="text" placeholder="John" id="clientFirstname" name="clientFirstname">
+                    <input type="text" placeholder="John" id="clientFirstname" name="clientFirstname" required>
                     <br>
                     <label for="clientLastname"><b>Last name</b></label><br>
-                    <input type="text" placeholder="Doe" id="clientLastname" name="clientLastname">
+                    <input type="text" placeholder="Doe" id="clientLastname" name="clientLastname" required>
                     <br>
                     <label for="clientEmail"><b>Email</b></label><br>
-                    <input type="text" placeholder="jhondoe@myemail.com" id="clientEmail" name="clientEmail">
+                    <input type="email" placeholder="jhondoe@myemail.com" id="clientEmail" name="clientEmail" required>
                     <br>
                     <p class="info-note">Passwords must be 8 characters and contain at least 1 number, 1 capital letter, and 1 special character.</p>
                     <label for="clientPassword"><b>Password</b></label><br>
-                    <input type="text" placeholder="Enter your password" id="clientPassword" name="clientPassword"><br>
+                    <input type="password" placeholder="Enter your password" id="clientPassword" name="clientPassword" required pattern="(?=^.{8,}$)(?=.*\d)(?=.*\W+)(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$"><br>
                     <label><input type="checkbox" name="showPassword">Show password</label>
                     <br>
                     <button type="submit" name="submit" id="regbtn" value="Register">Register</button>

a0ca24bfd13b9daea67e69e309656435ccf2f7fd
lop19029 <alexhumbertolds@gmail.com>
2021-06-11 23:14:42 -0300

    create register cookie for clientfirstname

--- phpmotors/accounts/index.php
+++ phpmotors/accounts/index.php
@@ -73,6 +73,7 @@
             
             //Check and report the result
             if($regOutcome === 1){
+                setcookie('firstname', $clientFirstname, strtotime('+1 year'), '/');
                 $message = "<p>Thanks for registering, $clientFirstname. Please use your email and password to login.</p>";
                 include '../view/login.php';
                 exit;

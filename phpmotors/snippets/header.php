3951ba0576550d02cedf2e276956a1f12b58c8c9
lop19029 <alexhumbertolds@gmail.com>
2021-06-13 00:01:48 -0300

    Add Logout button to other views

--- phpmotors/snippets/header.php
+++ phpmotors/snippets/header.php
@@ -3,7 +3,11 @@
     <?php 
         if(isset($_SESSION['loggedin'])){
             $userName = $_SESSION['clientData']['clientFirstname'];
-            echo "<span><a href='/CS%20340/phpmotors/accounts'> Logged as: $userName</a></span>";
+            echo "<span><a href='/CS%20340/phpmotors/accounts'> Welcome $userName</a></span>";
+            echo "<a href='/CS%20340/phpmotors/accounts/?action=Logout'>Logout</a>";
         }
-    ?><a href="/CS%20340/phpmotors/accounts/index.php?action=Login">My Account</a>
+        else {
+            echo '<a href="/CS%20340/phpmotors/accounts/?action=Login">My Account</a>';
+        }
+    ?>
 </header>

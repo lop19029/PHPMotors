c2694916ca1965b2fcf100faecc73eccc88c1b7d
lop19029 <alexhumbertolds@gmail.com>
2021-06-19 09:01:50 -0300

    Create getInventoryByClassification function

--- phpmotors/model/vehicles-model.php
+++ phpmotors/model/vehicles-model.php
@@ -56,4 +56,17 @@ function regCar($invMake, $invModel, $invDescription, $invImage, $invThumbnail, 
     return $rowsChanged;
 }
 
+
+// Get vehicles by classificationId 
+function getInventoryByClassification($classificationId){ 
+    $db = phpmotorsConnect(); 
+    $sql = ' SELECT * FROM inventory WHERE classificationId = :classificationId'; 
+    $stmt = $db->prepare($sql); 
+    $stmt->bindValue(':classificationId', $classificationId, PDO::PARAM_INT); 
+    $stmt->execute(); 
+    $inventory = $stmt->fetchAll(PDO::FETCH_ASSOC); 
+    $stmt->closeCursor(); 
+    return $inventory; 
+   }
+
 ?>

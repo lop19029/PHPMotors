bdc98941b19f223c88401b9bd55db5ec2a1224a9
lop19029 <alexhumbertolds@gmail.com>
2021-06-19 09:15:14 -0300

    Update vehicles management view to display table

--- phpmotors/view/vehicleManagement.php
+++ phpmotors/view/vehicleManagement.php
@@ -31,8 +31,23 @@ elseif(isset($_SESSION['loggedin'])){
                     <li><a href="/CS%20340/phpmotors/vehicles/?action=Classification">Add a new Classification</a></li>
                     <li><a href="/CS%20340/phpmotors/vehicles/?action=Vehicle">Add Vehicle</a></li>
                 </ul>
+                <?php
+                    if (isset($message)) { 
+                    echo $message; 
+                    } 
+                    if (isset($classificationList)) { 
+                    echo '<h2>Vehicles By Classification</h2>'; 
+                    echo '<p>Choose a classification to see those vehicles</p>'; 
+                    echo $classificationList; 
+                    }
+                ?>
+                <noscript>
+                    <p><strong>JavaScript Must Be Enabled to Use this Page.</strong></p>
+                </noscript>
+                <table id="inventoryDisplay"></table>
             </main>
             <?php require $_SERVER['DOCUMENT_ROOT'] . '/CS 340/phpmotors/snippets/footer.php'; ?>
         </div>
     </body>
+    <script src="../js/inventory.js"></script>
 </html>

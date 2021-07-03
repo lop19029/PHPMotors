6d40e21dc2f7a6f931f65566734fb34625d1695d
lop19029 <alexhumbertolds@gmail.com>
2021-07-03 11:59:18 -0300

    Create function that get all vehicles in inventory

--- phpmotors/model/vehicles-model.php
+++ phpmotors/model/vehicles-model.php
@@ -150,4 +150,16 @@ function getVehiclesByClassification($classificationName){
     $stmt->closeCursor();
     return $vehicles;
 }
+
+//This function will obtain about all vehicles in inventory
+function getVehicles(){
+	$db = phpmotorsConnect();
+	$sql = 'SELECT invId, invMake, invModel FROM inventory';
+	$stmt = $db->prepare($sql);
+	$stmt->execute();
+	$invInfo = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	$stmt->closeCursor();
+	return $invInfo;
+}
+
 ?>

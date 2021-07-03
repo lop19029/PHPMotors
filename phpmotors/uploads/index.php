16e0fd030a883a792182ebcb2d0649854d0366a3
lop19029 <alexhumbertolds@gmail.com>
2021-07-03 11:28:36 -0300

    Handle delete images

--- phpmotors/uploads/index.php
+++ phpmotors/uploads/index.php
@@ -23,7 +23,7 @@ if ($action == NULL) {
 * Variables for use with the Image Upload Functionality
 * **************************************************** */
 // directory name where uploaded images are stored
-$image_dir = '/phpmotors/uploads/images';
+$image_dir = '/CS%20340/phpmotors/uploads/images';
 // The path is the full path from the server root
 $image_dir_path = $_SERVER['DOCUMENT_ROOT'] . $image_dir;
 
@@ -64,9 +64,55 @@ switch ($action) {
         header('location: .');
         break;
     case 'delete':
-    
+        // Get the image name and id
+        $filename = filter_input(INPUT_GET, 'filename', FILTER_SANITIZE_STRING);
+        $imgId = filter_input(INPUT_GET, 'imgId', FILTER_VALIDATE_INT);
+            
+        // Build the full path to the image to be deleted
+        $target = $image_dir_path . '/' . $filename;
+            
+        // Check that the file exists in that location
+        if (file_exists($target)) {
+        // Deletes the file in the folder
+        $result = unlink($target); 
+        }
+            
+        // Remove from database only if physical file deleted
+        if ($result) {
+        $remove = deleteImage($imgId);
+        }
+            
+        // Set a message based on the delete result
+        if ($remove) {
+        $message = "<p class='notice'>$filename was successfully deleted.</p>";
+        } else {
+        $message = "<p class='notice'>$filename was NOT deleted.</p>";
+        }
+            
+        // Store message to session
+        $_SESSION['message'] = $message;
+            
+        // Redirect to this controller for default action
+        header('location: .');
         break;
     default:
-
+        // Call function to return image info from database
+        $imageArray = getImages();
+            
+        // Build the image information into HTML for display
+        if (count($imageArray)) {
+        $imageDisplay = buildImageDisplay($imageArray);
+        } else {
+        $imageDisplay = '<p class="notice">Sorry, no images could be found.</p>';
+        }
+            
+        // Get vehicles information from database
+        $vehicles = getVehicles();
+        // Build a select list of vehicle information for the view
+        $prodSelect = buildVehiclesSelect($vehicles);
+            
+        include '../view/image-admin.php';
+        exit;
+        
         break;
 }
